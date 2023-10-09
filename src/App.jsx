2535e625f8cdfbbabef30d20768b40404b5763e0
Spencer Peterson <spencerpeterson.wi@gmail.com>
2023-10-09 11:53:09 -0600

    Did steps 1 and 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import playerData from "./playerData.js";
+
 function BaseballCard(props) {
   return (
     <div className="card">
-      <h2>Player Name goes here</h2>
+      <h2>{props.player.name}</h2>
+      <img src={props.player.imgUrl} alt={props.player.name} />
     </div>
   );
 }
 
 function App() {
-  return <BaseballCard />;
+  // for each player in player data, call the BaseballCard function
+  // the BaseballCard function creates a card div with a playerName
+  const cards = playerData.map(player => 
+    <BaseballCard player={player} />
+  );
+  // return that list of elements 
+  return <>{cards}</>;
 }
 
 export default App;
